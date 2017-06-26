8e4eba1e47119d634f59273c8c13ed304e16d8dc
Derek Neuts <dcneuts@gmail.com>
2017-06-25 22:53:52 -0700

    JetBrains config files and app.js update to allow 'responded' class to be applied when checkbox is checked off for attendees

--- app.js
+++ app.js
@@ -3,13 +3,31 @@
  */
 const FORM = document.getElementById('registrar');
 const INPUT = FORM.querySelector('input');
+const UL = document.getElementById('invitedList');
 
 FORM.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const TEXT = INPUT.value;
 	INPUT.value = "";
-	const UL = document.getElementById('invitedList');
 	const LI = document.createElement('li');
 	LI.textContent = TEXT;
+	const LABEL = document.createElement('label');
+	LABEL.textContent = 'Confirmed';
+	const CHECKBOX = document.createElement('input');
+	CHECKBOX.type = 'checkbox';
+	LABEL.appendChild(CHECKBOX);
+	LI.appendChild(LABEL);
 	UL.appendChild(LI);
+});
+
+UL.addEventListener('change', () => {
+	const CHECKBOX = event.target;
+	const CHECKED = CHECKBOX.checked;
+	const LISTITEM = CHECKBOX.parentNode.parentNode;
+	
+	if (CHECKED) {
+		LISTITEM.className = 'responded';
+	} else {
+		LISTITEM.className = '';
+	}
 });
