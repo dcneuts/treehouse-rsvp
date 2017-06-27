434d6a073b3d798fffc92e57700897074670b2f8
Derek Neuts <dcneuts@gmail.com>
2017-06-26 21:27:05 -0700

    Implemented save by adding additional ELSE IF statement and reversing the steps from the 'edit' ELSE IF routine

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 /**
  * Created by derek on 6/25/17.
  */
-const FORM = document.getElementById('registrar');
+const FORM  = document.getElementById('registrar');
 const INPUT = FORM.querySelector('input');
-const UL = document.getElementById('invitedList');
+const UL    = document.getElementById('invitedList');
 
 function createLI(TEXT) {
-	const LI = document.createElement('li');
-	const SPAN = document.createElement('span');
+	const LI         = document.createElement('li');
+	const SPAN       = document.createElement('span');
 	SPAN.textContent = TEXT;
 	LI.appendChild(SPAN);
-	const LABEL = document.createElement('label');
+	const LABEL       = document.createElement('label');
 	LABEL.textContent = 'Confirmed';
-	const CHECKBOX = document.createElement('input');
-	CHECKBOX.type = 'checkbox';
+	const CHECKBOX    = document.createElement('input');
+	CHECKBOX.type     = 'checkbox';
 	LABEL.appendChild(CHECKBOX);
 	LI.appendChild(LABEL);
-	
-	const EDITBUTTON = document.createElement('button');
+
+	const EDITBUTTON       = document.createElement('button');
 	EDITBUTTON.textContent = 'edit';
 	LI.appendChild(EDITBUTTON);
-	
-	const REMOVEBUTTON = document.createElement('button');
+
+	const REMOVEBUTTON       = document.createElement('button');
 	REMOVEBUTTON.textContent = 'remove';
 	LI.appendChild(REMOVEBUTTON);
 	return LI;
@@ -30,18 +30,18 @@ function createLI(TEXT) {
 // Event handler to add people to list on button press
 FORM.addEventListener('submit', (e) => {
 	e.preventDefault();
-	const TEXT = INPUT.value;
+	const TEXT  = INPUT.value;
 	INPUT.value = "";
-	const LI = createLI(TEXT);
+	const LI    = createLI(TEXT);
 	UL.appendChild(LI);
 });
 
 // Event handler to change styles
 UL.addEventListener('change', () => {
 	const CHECKBOX = event.target;
-	const CHECKED = CHECKBOX.checked;
+	const CHECKED  = CHECKBOX.checked;
 	const LISTITEM = CHECKBOX.parentNode.parentNode;
-	
+
 	if (CHECKED) {
 		LISTITEM.className = 'responded';
 	} else {
@@ -53,17 +53,25 @@ UL.addEventListener('click', (e) => {
 	// To stop the edit button from being triggered due to it's position in the DOM
 	if (e.target.tagName === 'BUTTON') {
 		const BUTTON = e.target;
-		const LI = BUTTON.parentNode;
-		const UL = LI.parentNode;
+		const LI     = BUTTON.parentNode;
+		const UL     = LI.parentNode;
 		if (BUTTON.textContent === 'remove') {
 			UL.removeChild(LI);
 		} else if (BUTTON.textContent === 'edit') {
-			const SPAN = LI.firstElementChild;
+			const SPAN  = LI.firstElementChild;
 			const INPUT = document.createElement('input');
-			INPUT.type = 'text';
+			INPUT.type  = 'text';
 			INPUT.value = SPAN.textContent;
 			LI.insertBefore(INPUT, SPAN);
 			LI.removeChild(SPAN);
-			BUTTON.textContent =  "save";
+			BUTTON.textContent = "save";
+		} else if (BUTTON.textContent === 'save') {
+			const INPUT      = LI.firstElementChild;
+			const SPAN       = document.createElement('span');
+			SPAN.textContent = INPUT.value;
+			LI.insertBefore(SPAN, INPUT);
+			LI.removeChild(INPUT);
+			BUTTON.textContent = "edit";
+		}
 	}
-}});
+});
